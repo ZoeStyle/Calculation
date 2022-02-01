525d832c7210792012271fe7884da3c717019ffd
Victor Aparecido Hernandes Santos <regiotimao@gmail.com>
2022-02-01 20:00:38 -0300

    realizando a troca de arquitetura do projeto
    apagando as pastas atuais

--- src/routes/formula-route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/formula-controller')
-
-router.post('/formula', controller.post);
-
-module.exports = router;
