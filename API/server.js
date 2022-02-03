2e063d2efb1e6ef921d08030b4d1ac075b88854c
Victor Aparecido Hernandes Santos <39266802+ZoeStyle@users.noreply.github.com>
2022-02-03 17:25:22 -0300

    Configuração da porta pois apresenta erro no heroku (#19)
    * Atualização do pipeline adicionando Coveralls
    
    * ataualização do dockerfile para nmp ci
    
    * Atualização para inserção da porta no heroku
    
    * Atualização da porta
    
    * Configuração da porta posi apresenta erro no heroku
    
    * Configuração da porta pois apresenta erro no heroku

--- API/server.js
+++ API/server.js
@@ -8,10 +8,10 @@ fastify.register(require('./routes/healthCheck.route'));
 
 const start = async () => {
     try {
-        const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-        const server_host = process.env.YOUR_HOST || '0.0.0.0';
-        await fastify.listen(server_port, server_host);
-        logger.info('The api is running in the pota' + port);
+        const port = normalize_port(process.env.PORT || 40100);
+
+        await fastify.listen(port);
+        logger.info('The api is running in the door' + port);
     } catch (err) {
         fastify.log.error(err);
         logger.error(err);
