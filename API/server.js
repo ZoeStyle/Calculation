1a7e2c8d6f3deff4f546f88eb360435f747018f6
Victor Aparecido Hernandes Santos <regiotimao@gmail.com>
2022-02-03 19:48:30 -0300

    Habilitando CORS na aplicação
    Ainda será configurado nivel de acesso para aplicação

--- API/server.js
+++ API/server.js
@@ -3,6 +3,11 @@
 const fastify = require('fastify')({ logger: true });
 const logger = require('../Logger/logger');
 
+fastify.register(require('fastify-cors'), {
+    origin: false,
+    methods: ['GET', 'PUT', 'POST'],
+});
+
 fastify.register(require('./routes/Calculation.route'));
 fastify.register(require('./routes/healthCheck.route'));
 
@@ -10,7 +15,7 @@ const start = async () => {
     try {
         const port = process.env.PORT;
 
-        await fastify.listen(port, '0.0.0.0');
+        await fastify.listen(port);
         logger.info('The api is running in the door' + port);
     } catch (err) {
         fastify.log.error(err);
