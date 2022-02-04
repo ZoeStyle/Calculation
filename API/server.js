2e7c2a4c1b0782db7df0ab690e191ff42ab0439a
Victor Aparecido Hernandes Santos <39266802+ZoeStyle@users.noreply.github.com>
2022-02-04 00:35:31 -0300

    Configuração CORS

--- API/server.js
+++ API/server.js
@@ -3,11 +3,6 @@
 const fastify = require('fastify')({ logger: true });
 const logger = require('../Logger/logger');
 
-fastify.register(require('fastify-cors'), {
-    origin: 'https://app.swaggerhub.com/apis/ZoeStyle/Hernandes/0.0.1#/Calculation/post_Bhaskara',
-    methods: ['POST', 'OPTIONS', 'GET']
-});
-
 fastify.register(require('./routes/Calculation.route'));
 fastify.register(require('./routes/healthCheck.route'));
 
@@ -15,7 +10,7 @@ const start = async () => {
     try {
         const port = process.env.PORT;
 
-        await fastify.listen(port);
+        await fastify.listen(port, 'https://app.swaggerhub.com/apis/ZoeStyle/Hernandes/0.0.1#/Calculation/post_Bhaskara');
         logger.info('The api is running in the door' + port);
     } catch (err) {
         fastify.log.error(err);
